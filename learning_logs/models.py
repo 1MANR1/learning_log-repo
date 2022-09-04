50dc6a992402407c85da712018bb82186d7c29fe
Iman <imanri2003@gmail.com>
2022-09-04 21:27:43 +0430

    Ready for deployment to heroku.

---
+++ learning_logs/models.py
@@ -0,0 +1,31 @@
+from cgitb import text
+from django.db import models
+from django.contrib.auth.models import User
+
+class Topic(models.Model):
+    """A topic the user is learning about"""
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        """Return a string represention of the model """
+        return self.text
+
+
+class Entry(models.Model):
+    """Somthing specific learned about a topic"""
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = 'entries'
+
+    def __str__(self):
+        """Return a string  represention of the model"""
+        if (len(self.text) > 50):
+            string = f"{self.text[:50]}..."
+        else:
+            string = f"{self.text[:50]}"
+        return string
